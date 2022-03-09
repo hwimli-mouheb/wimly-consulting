884538cb130ea4fbc127e9f5322f51c05830671f
hwimli.mouheb <mohamedmouhebhouimli@insat.u-carthage.tn>
2022-03-09 19:37:17 +0100

    making changes

--- src/components/pages/Imprint/imprint.js
+++ src/components/pages/Imprint/imprint.js
@@ -28,9 +28,9 @@ function imprint() {
       <p style={{height:'508px'}}> <h5>Wimly Consulting SARL </h5> represented by Najiba Raddadi Houimli
 <br></br>Address: 1 Rue Amazone, Jardin Menzah 2, Ariana, Tunisie.
 <br></br>Tax registration number: 1752523/N
-<br></br>Email: houimli@spacetec.partners
+<br></br>Email: wimly-consulting@gmail.com
 <br></br>Phone number: +49 176 45689565
-<br></br><a style={{color:'#09371f'}} href="https://calendly.com/houimli/wimly-consulting">Schedule a meeting</a> 
+<br></br><a href="https://calendly.com/houimli/wimly-consulting">Schedule a meeting</a> 
 </p>
 
     
